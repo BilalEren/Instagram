08b6b20057c83f1b551ad33ea04802ffd4fd1bee
Bilal Eren <developer.bilaleren@gmail.com>
2018-03-31 17:25:56 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="tr">
 <head>
@@ -9,8 +8,8 @@
     <script type="text/javascript">
     
         $(document).ready(function(){
-            var client_id = '';//instagram developer console da yapılan uygulama idsi.
-            var uri = '';//instagram developer console da verdiginiz redirect uri localde de calısır.       
+            var client_id = 'ff91d1509c6546d7b65ca696ee4dabb3';//instagram developer console da yapılan uygulama idsi.
+            var uri = 'http://localhost/projeler/ins/index.php';//instagram developer console da verdiginiz redirect uri localde de calısır.       
             
             if(!window.location.hash.match('access')){
                 $('#token').html('<a href="https://www.instagram.com/oauth/authorize/?client_id='+client_id+'&redirect_uri='+uri+'&response_type=token&scope=public_content">Token</a>');
@@ -19,6 +18,39 @@
             var has = window.location.hash.slice(1);//urlden gelen access_token hasini alıyoruz.
             var token = has.replace(/access_token=/gi,'');//hasin basında olan access_token= adlı kısmı degistiriyoruz
             
+            
+            //Profil bilgilerine erişme
+            $.ajax({
+                url: 'https://api.instagram.com/v1/users/self/?access_token='+token,
+                dataType: 'json',
+                type: 'GET',
+                success: function(data){
+                    var html = '';
+                    console.log(data);
+                    html += '<p><img src="'+data.data.profile_picture+'" style="border-radius: 50%" width="50px" height="50px"></p>';
+                    html += '<p>İsim: '+data.data.full_name+'</p>';
+                    html += '<p>Kullanıcı Adı: '+data.data.username+'</p>';
+                    if(data.data.website != ""){
+                        html += '<p> Website: '+'<a target="_blank" href="'+data.data.website+'">'+data.data.website+'</a>'+'</p>'
+                    } else {
+                        html += '<p>Website: Websitesi yok.</p>';
+                    }
+                    if(data.data.bio != ""){
+                        html += '<p>Biografi: '+data.data.bio+'</p>';
+                    } else {
+                        html += '<p>Biografi: Biografi yok.</p>';
+                    }
+                    if(data.data.counts.media != ""){
+                        html += '<p>Medya: '+data.data.counts.media+'</p>';
+                    } else {
+                        html += '<p>Medya: Paylaşılan fotoğraf veya video bulunamadı.</p>';
+                    }
+                    html += '<p>Takipçi: '+data.data.counts.followed_by+'</p>';
+                    html += '<p>Taki Edilen: '+data.data.counts.follows+'</p>';
+                    
+                    $('#bilgi').append(html);
+                }
+            });
             
             //Instagram verisini alarak sayfamızda gösterme.
             $.ajax({
@@ -65,10 +97,28 @@
                             var baslik = "";
                         }
                         
+                        var tarih = new Date(veri[i].created_time * 1000),
+                            gun = tarih.getDate(),
+                            ay = tarih.getMonth(),
+                            yil = tarih.getFullYear();
+                        
+                        var aylar = ["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"];
+                        var gunler = ["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
+            
+                        var tarihstr =  gun + ' ' + aylar[ay] + ' ' + yil + ' ' + gunler[tarih.getDay()];
+                        
                         html += '<div>';
-                        html += '<img src="'+veri[i].images.standard_resolution.url+'" width="400px" height="400px">';
-                        html += '<p>'+caption(baslik)+'</p>';
-                        html += '</div>';
+                        if(veri[i].type == "image" || !veri[i].type == "video"){
+                            html += '<img src="'+veri[i].images.standard_resolution.url+'" width="400px" height="400px">';
+                        } else if(veri[i].type == "video" || !veri[i].type == "image"){
+                            html += '<video width="400px" height="400px" controls poster="'+veri[i].images.standard_resolution.url+'">';
+                            html += '<source src="'+veri[i].videos.standard_resolution.url+'" type="video/mp4">';
+                            html += '</video>';
+                        }
+                        html += '<a href="'+veri[i].link+'" target="_blank"><p>'+caption(baslik)+'</p></a>';
+                        html += '<p><span>Beğeni: '+veri[i].likes.count+ ' <span>Yorum:'+veri[i].comments.count+'</span></p>';
+                        html += '<p>'+tarihstr+'</p>';
+                        html += '</div><hr/>';
                     }
                     
                     $('#data').append(html);
@@ -91,7 +141,7 @@
                 return false;
             });
             
-            $('button').trigger('click');//sayfa yüklenir yüklenme butona tıklama işlemi gerçekleşti.
+            $('button').trigger('click');//sayfa yüklenir yüklenmez butona tıklama işlemi gerçekleşti.
         });
         
     </script>
@@ -99,6 +149,7 @@
 </head>
 <body>
     <div id="token"></div>
+    <div id="bilgi"></div>
     <div id="data"></div>
     
     <form method="post" style="display: none;">
